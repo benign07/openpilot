d052ae086df505c2f63d85c46a91e473843c7c7a
benign07 <138122245+benign07@users.noreply.github.com>
2024-04-14 13:12:36 +0900

    Delete openpilot directory

--- openpilot/common/model.h
+++
@@ -1,1 +0,0 @@
-#define CURRENT_MODEL "Certified Herbalist v2 (February 13, 2024)"
--- openpilot/common/time.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-MIN_DATE = datetime.datetime(year=2024, month=1, day=28)
-
-def system_time_valid():
-  return datetime.datetime.now() > MIN_DATE
