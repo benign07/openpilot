e308e20534b9a52de85c45927363edbb6a746d47
benign07 <138122245+benign07@users.noreply.github.com>
2025-12-03 07:25:10 +0900

    Update values.py

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -471,7 +471,7 @@ class CAR(Platforms):
     # flags=HyundaiFlags.HYBRID | HyundaiFlags.ANGLE_CONTROL | HyundaiFlags.CANFD_HDA2,
     # [수정 후 - 이걸로 하세요!]
     #flags=HyundaiFlags.HYBRID | HyundaiFlags.ANGLE_CONTROL | HyundaiFlags.SEND_LFA,
-    flags=HyundaiFlags.HYBRID | HyundaiFlags.CANFD_HDA2 | HyundaiFlags.ANGLE_CONTROL | HyundaiFlags.CANFD_ALT_GEARS_2,
+    flags=HyundaiFlags.HYBRID | HyundaiFlags.CANFD_HDA2 | HyundaiFlags.ANGLE_CONTROL,
   )
 
   # Kia
