d0f43a7f96be2599bff841039cc266c781890c21
benign07 <138122245+benign07@users.noreply.github.com>
2026-02-21 17:50:04 +0900

    Update carstate.py

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -425,9 +425,13 @@ class CarState(CarStateBase):
     ret.brakePressed = cp.vl["TCS"]["DriverBraking"] == 1
     #print(cp.vl["TCS"], cp.vl_all["TCS"]["DriverBraking"][-10:])
 
-    ret.doorOpen = cp.vl["DOORS_SEATBELTS"]["DRIVER_DOOR"] == 1
-    ret.seatbeltUnlatched = cp.vl["DOORS_SEATBELTS"]["DRIVER_SEATBELT"] == 0
+    #기존코드 -> 벨트 도어 분리
+    #ret.doorOpen = cp.vl["DOORS_SEATBELTS"]["DRIVER_DOOR"] == 1
+    #ret.seatbeltUnlatched = cp.vl["DOORS_SEATBELTS"]["DRIVER_SEATBELT"] == 0
 
+    ret.doorOpen = cp.vl["DOORS"]["DRIVER_DOOR"] == 1
+    ret.seatbeltUnlatched = cp.vl["SEATBELTS"]["DRIVER_SEATBELT"] == 0    
+        
     gear = cp.vl[self.gear_msg_canfd]["GEAR"]
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(gear))
 
